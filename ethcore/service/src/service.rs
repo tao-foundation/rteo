8f6ec63e55d6be582b71d3fbd13015da03bc23ec
trustfarm-dev <trustfarm.info@gmail.com>
2018-11-22 13:31:19 +0900

    Version 1.12.3 - updates public mainnet bootnode and remove compile warn

--- ethcore/service/src/service.rs
+++ ethcore/service/src/service.rs
@@ -89,7 +89,7 @@ impl ClientService {
 		) -> Result<ClientService, Error>
 	{
 		let io_service = IoService::<ClientIoMessage>::start()?;
-		/// change PoW algorithm changed engine name
+		// change PoW algorithm changed engine name
 		let engine_name = "TEthashV1";
 		
 		info!("Configured for {} using {} engine", Colour::White.bold().paint(spec.name.clone()), Colour::Yellow.bold().paint(engine_name));
